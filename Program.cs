e97d3aea7394fe74bd269dc439df28cf989bf3a5
wortelus <daniel.slavik.cz@gmail.com>
2018-10-31 22:43:37 +0100

    made a solver recursive algorithm

--- Program.cs
+++ Program.cs
@@ -10,23 +10,90 @@ namespace sudoku
     {
         static void Main(string[] args)
         {
-            Game g = new Game();
+            int[,] grid = new int[9,9];
+
+            //experimental grid
+            grid[0, 0] = 1;
+            grid[1, 0] = 2;
+            grid[4, 4] = 5;
+            grid[6, 6] = 5;
+            grid[8, 1] = 3;
+            grid[1, 4] = 9;
+            grid[8, 8] = 6;
+            grid[0, 8] = 2;
+            Game g = new Game(grid);
             Console.ReadLine();
         }
     }
 
     class Game
     {
-        const int difficulty = 13; //edit this variable for defining the probability of clear values
+        int difficulty = 4; //edit this variable for defining the probability of clear values
         int[,] grid = new int[9, 9];
         Random r = new Random();
         int cycles = 0;
         int calls = 0;
+        List<Tuple<int, int>> gridSolveValues = new List<Tuple<int, int>>();
 
         public Game()
         {
             GenerateRecursive(0, 0);
             RenderGrid();
+        }
+
+        /// <summary>
+        /// constructor for solving pre-defined sudoku boards
+        /// </summary>
+        /// <param name="inputGrid"></param>
+        public Game(int[,] inputGrid)
+        {
+            difficulty = 0; //to show completed board
+            grid = inputGrid;
+            for (int y = 0; y < 9; y++)
+            {
+                for (int x = 0; x < 9; x++)
+                {
+                    if (grid[x, y] == 0)
+                    {
+                        Tuple<int, int> value = new Tuple<int, int>(x, y);
+                        gridSolveValues.Add(value);
+                    }
+                }
+            }
+            SolveRecursive(gridSolveValues[0].Item1, gridSolveValues[0].Item2);
+            RenderGrid();
+        }
+
+        private bool SolveRecursive(int x, int y)
+        {
+            calls++;
+            for (int i = 1; i <= 9; i++)
+            {
+                bool block = CheckBlockValue(x / 3, y / 3, i);
+                bool row = CheckRow(y, i);
+                bool column = CheckColumn(x, i);
+                if (block == true && row == true && column == true)
+                {
+                    grid[x, y] = i;
+                    Tuple<int, int> nextPosition = GetNextSolvePosition(x, y);
+                    if (nextPosition == null)
+                    {
+                        return true;
+                    }
+                    bool nextIteration = SolveRecursive(nextPosition.Item1, nextPosition.Item2);
+                    if (nextIteration == true)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                cycles++;
+            }
+            grid[x, y] = 0;
+            return false;
         }
 
         private void RenderGrid()
@@ -110,6 +177,21 @@ namespace sudoku
             }
         }
 
+        private Tuple<int, int> GetNextSolvePosition(int x, int y)
+        {
+            for (int i = 0; i < gridSolveValues.Count; i++)
+            {
+                if (i == gridSolveValues.Count - 1)
+                {
+                    return null;
+                }
+                else if (gridSolveValues[i].Item1 == x && gridSolveValues[i].Item2 == y)
+                {
+                    return gridSolveValues[i + 1];
+                }
+            }
+            throw new Exception("couldn't find next position in solve grid, wrong axis coordinations");
+        }
 
         private int[] LoadBlockValues(int x, int y)
         {
